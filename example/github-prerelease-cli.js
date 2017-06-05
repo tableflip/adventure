dfa9cc861802d43475784febcead7e03d30c4cc0
Richard Silverton <silverton.richard@googlemail.com>
2017-06-05 17:25:38 +0100

    Adds CLI example

--- example/github-prerelease-cli.js
+++ example/github-prerelease-cli.js
@@ -1,17 +1,17 @@
 /*
-github-interface-cli
+github-prerelease-cli
 
 Make sure you've got a github.rc in the project root with your username and an api key.
 
 USAGE:
-  node example/github-interface-cli.js --repo olizilla/tags --version v2.0.0 --message "good one"
+  node example/github-prerelease-cli.js --repo olizilla/tags --version v2.0.0 --message "good one"
 
 */
 const {prerelease} = require('../lib/github-interface')
 const argv = require('minimist')(process.argv.slice(2))
 
 function usage () {
-  console.log(`USAGE: node example/github-interface-cli.js --repo org/repo --version v2.0.0 --message 'Best release ever'`)
+  console.log(`USAGE: node example/github-prerelease-cli.js --repo org/repo --version v2.0.0 --message 'Best release ever'`)
 }
 
 const { repo, version, message } = argv
