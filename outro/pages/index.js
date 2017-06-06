f7f8536de92371a9e9054c652034d51a1c18b05b
Bernard Mordan <bernardmordan@gmail.com>
2017-06-06 14:48:38 +0100

    foundations for next

---
+++ outro/pages/index.js
@@ -0,0 +1,9 @@
+export default () => (
+  <svg width='60' height='66' viewBox='0 0 99 109' xmlns='http://www.w3.org/2000/svg'>
+    <title>TABLEFLIP</title>
+    <g stroke='#878787' strokeLinecap='round' strokeLinejoin='round' fill='none' fillRule='evenodd'>
+      <path d='M2 28.5L49.5 2 97 28v52.5l-46.5 27-48-26-.5-53z' strokeWidth='3' />
+      <path d='M49 15.5L12.5 34 49 53l37.5-19L49 15.5zm-36.5 19v31m37-12v30m38-49v30' strokeWidth='5' />
+    </g>
+  </svg>
+)
