986d2020cdff308ac91ec2a54098847a9a7ac507
Bernard Mordan <bernardmordan@gmail.com>
2017-06-09 16:31:30 +0100

    removes miss cased file names

--- outro/components/icons/Svg.js
+++
@@ -1,5 +0,0 @@
-export default (props) => (
-  <svg width={props.width || '14'} height={props.height || '14'} viewBox='0 0 24 24' transform={`translate(${props.translate || '0, 0'})`}>
-    {props.children}
-  </svg>
-)
--- outro/components/lists/ListItem.js
+++
@@ -1,5 +0,0 @@
-export default ({children = null, className = ''}) => (
-  <div className={`flex items-center justify-start bg-black-10 hover-bg-black-20 pa2 ${className}`}>
-    {children}
-  </div>
-)
